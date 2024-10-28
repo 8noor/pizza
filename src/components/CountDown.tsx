eb33c1ad75123412a6d27b2f7c766d4b90210ba9
8noor <nanum3773@gmail.com>
2024-10-28 16:35:07 +0500

    first commit

---
+++ src/components/CountDown.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const CountDown: React.FC = () => {
+  const endDate = new Date("2023-10-10").getTime();
+
+  const calculateTimeLeft = (): TimeLeft => {
+    const now = new Date().getTime();
+    const difference = endDate - now;
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  };
+
+  const [timeLeft, setTimeLeft] = useState<TimeLeft>(calculateTimeLeft);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <span className="font-bold text-5xl text-yellow-300">
+      {timeLeft.days}:{timeLeft.hours}:{timeLeft.minutes}:{timeLeft.seconds}
+    </span>
+  );
+};
+
+export default CountDown;
